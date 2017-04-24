2935fc0a106206dee13391d34bdfb59d82a0aefc
ritzesrepo <ritzesrepo@gmail.com>
2017-04-24 08:59:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-    
+    var currentTime = new Date ( );
+    var currentHours = currentTime.getHours ( );
+    var currentMinutes = currentTime.getMinutes ( );
 });
