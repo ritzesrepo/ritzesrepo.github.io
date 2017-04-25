e72b1fa2903a2715ba9739efb7a862b51dfcbd35
ritzesrepo <ritzesrepo@gmail.com>
2017-04-25 08:56:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-    var currentTime = new Date ( );
-    var currentHours = currentTime.getHours ( );
-    var currentMinutes = currentTime.getMinutes ( )
-    function clockFunction() {
-        if (currentMinutes < 10) {
-            $("#clock").html(currentHours + " : 0" + currentMinutes);
-        } else {
-            $("#clock").html(currentHours + " : " + currentMinutes);
-        }
-    }
+    
 });
+
+var currentTime = new Date ( );
+var currentHours = currentTime.getHours ( );
+var currentMinutes = currentTime.getMinutes ( )
+function clockFunction() {
+    if (currentMinutes < 10) {
+        $("#clock").html(currentHours + " : 0" + currentMinutes);
+    } else {
+        $("#clock").html(currentHours + " : " + currentMinutes);
+    }
+}
