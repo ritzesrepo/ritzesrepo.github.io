7f51bf3cb97ffeab5a93887af157237a7422e877
ritzesrepo <ritzesrepo@gmail.com>
2017-04-25 12:44:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-$(document).ready(function() {
-    
-});
 
-var currentTime = new Date ( );
-var currentHours = currentTime.getHours ( );
-var currentMinutes = currentTime.getMinutes ( )
-function clockFunction() {
-    if (currentMinutes < 10) {
-        $("#clock").html("It is " + currentHours + " : 0" + currentMinutes);
-    } else {
-        $("#clock").html("It is " + currentHours + " : " + currentMinutes);
-    }
-}
